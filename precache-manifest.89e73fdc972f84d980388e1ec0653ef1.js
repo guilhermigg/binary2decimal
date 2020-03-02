1159c3d1e6ca0e4d097d5a5210ebf21be9c2f0c0
Guilhermigg <guilhermigg.e@gmail.com>
2020-03-02 00:03:37 -0300

    Updates

---
+++ precache-manifest.89e73fdc972f84d980388e1ec0653ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1236701c1c4cd1f18ba0721fa49be441",
+    "url": "/bin2dec-converter/index.html"
+  },
+  {
+    "revision": "9a99ec2d266ce79dc950",
+    "url": "/bin2dec-converter/static/css/main.022831dc.chunk.css"
+  },
+  {
+    "revision": "20cec83f53ac19f439a6",
+    "url": "/bin2dec-converter/static/js/2.26acce97.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bin2dec-converter/static/js/2.26acce97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a99ec2d266ce79dc950",
+    "url": "/bin2dec-converter/static/js/main.8d4de31f.chunk.js"
+  },
+  {
+    "revision": "6e23c9b4a0482757654c",
+    "url": "/bin2dec-converter/static/js/runtime-main.b2feeba9.js"
+  }
+]);
